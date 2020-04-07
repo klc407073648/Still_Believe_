0235acf014c5ef0174b33ba710f7a5a16665991c
klc407073648 <407073648@qq.com>
2020-04-08 07:32:38 +0800

    修改nocopyable类

--- webServerStudy/V4/HttpServer/base/nocopyable.hpp
+++ webServerStudy/V4/HttpServer/base/nocopyable.hpp
@@ -17,9 +17,11 @@ class noncopyable
 protected:
     //默认的构造函数和析构函数是保护的
     //使用编译器的自动产生的默认实现
+	//不允许创建 noncopyable 实例，但允许子类创建实例（即允许派生类构造和析构）。
     noncopyable() = default;
     ~noncopyable() = default;
- 
+
+private:
     //使用delete关键字禁止编译器自动产生复制构造函数和复制赋值操作符
     noncopyable( const noncopyable& ) = delete;
     const noncopyable& operator=( const noncopyable& ) = delete;
