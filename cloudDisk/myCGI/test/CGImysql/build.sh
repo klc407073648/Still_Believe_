8c508ff671a9c07869c79dc528cfdf8c7b3ff0ef
klc407073648 <407073648@qq.com>
2020-07-18 21:46:42 +0800

    fastcgi学习，返回html网页

--- cloudDisk/myCGI/test/CGImysql/build.sh
+++ cloudDisk/myCGI/test/CGImysql/build.sh
@@ -1,3 +1,7 @@
 g++ -o ./register ./register.cpp  -I /usr/include/mysql -L /usr/lib64/mysql -lmysqlclient -lpthread -lfcgi -std=c++11
 g++ -o ./login ./login.cpp  -I /usr/include/mysql -L /usr/lib64/mysql -lmysqlclient -lpthread -lfcgi -std=c++11
 g++ upload.cpp -o upload -lfcgi -std=c++11
+
+spawn-fcgi  -a 127.0.0.1 -p 8050 -f ./register
+spawn-fcgi  -a 127.0.0.1 -p 8051 -f ./login
+spawn-fcgi  -a 127.0.0.1 -p 8052 -f ./upload
