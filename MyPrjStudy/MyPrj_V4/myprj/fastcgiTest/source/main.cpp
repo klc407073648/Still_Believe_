c56511dfde524199da2c20e1bb99d6d646cd358c
klc407073648 <407073648@qq.com>
2020-07-25 08:41:19 +0800

    学习upload功能，但是bufpost有问题

---
+++ MyPrjStudy/MyPrj_V4/myprj/fastcgiTest/source/main.cpp
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "Event.h"
+
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+	char **initialEnv = environ;
+    int count = 0;
+
+    Event mEvent(0);
+    //Login mLogin;
+    Register mReg;
+	Login mLogin;
+    Upload mUpload;
+    mEvent.addEvent("/register", eventBind(Register::reg, mReg));
+	mEvent.addEvent("/login", eventBind(Login::login, mLogin));
+    mEvent.addEvent("/upload", eventBind(Upload::upload, mUpload));
+    mEvent.exec();
+
+	return 0;
+}
